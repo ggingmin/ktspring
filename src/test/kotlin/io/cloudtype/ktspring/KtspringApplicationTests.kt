ce4e38348c3e30ee2a86c0a4553128e673d24dbd
KYEONGMIN CHO <ggingmin@gmail.com>
2023-04-24 11:28:28 +0900

    first commit

---
+++ src/test/kotlin/io/cloudtype/ktspring/KtspringApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.cloudtype.ktspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtspringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
